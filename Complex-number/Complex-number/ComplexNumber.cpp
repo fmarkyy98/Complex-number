f78254bc1a8f55d93aa72ba1494db14269ecbd48
Fekete Márk <fmark98@gmail.com>
2020-02-27 22:10:54 +0100

    .h abstraction done.

--- Complex-number/Complex-number/ComplexNumber.cpp
+++ Complex-number/Complex-number/ComplexNumber.cpp
@@ -1,1 +1,3 @@
 #include "ComplexNumber.h"
+
+
--- Complex-number/Complex-number/ComplexNumber.h
+++ Complex-number/Complex-number/ComplexNumber.h
@@ -1,5 +1,22 @@
 #pragma once
 class ComplexNumber
 {
+private:
+	double _real;
+	double _imag;
+
+public:
+	ComplexNumber();
+	ComplexNumber(double real);
+	ComplexNumber(double real, double imag);
+	ComplexNumber(ComplexNumber& c);
+
+	double getReal() const;
+	double getImag() const;
+	void setReal(double real);
+	void setImag(double imag);
+
+	ComplexNumber& add(ComplexNumber& c);
+	ComplexNumber& mul(ComplexNumber& c);
 };
 
