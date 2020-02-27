f6cb9f6facc2db82ffa51f4aeeba6da3f505b07e
Fekete Márk <fmark98@gmail.com>
2020-02-27 22:15:14 +0100

    Constructors done.

--- Complex-number/Complex-number/ComplexNumber.cpp
+++ Complex-number/Complex-number/ComplexNumber.cpp
@@ -1,3 +1,8 @@
 #include "ComplexNumber.h"
 
+ComplexNumber::ComplexNumber() : _real(), _imag() { }
+ComplexNumber::ComplexNumber(double real) : _real(real), _imag() { }
+ComplexNumber::ComplexNumber(double real, double imag) : _real(real), _imag(imag) { }
+ComplexNumber::ComplexNumber(ComplexNumber& c) : _real(c.getReal()), _imag(c.getImag) { }
+
 
